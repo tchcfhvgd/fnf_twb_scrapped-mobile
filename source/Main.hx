8c5dd94653414a369f509fc44839dd5f99a8fc13
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-23 15:17:46 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -87,7 +87,6 @@ class Main extends Sprite
 
 	private function setupGame():Void
 	{
-		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
