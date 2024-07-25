d120c810aece1666944c0e224f3dbd48bdb0f882
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-25 13:06:07 +0800

    Update CreditsState.hx

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -243,6 +243,9 @@ class CreditsState extends MusicBeatState
 		bg.color = CoolUtil.colorFromString(creditsStuff[curSelected][4]);
 		intendedColor = bg.color;
 		changeSelection();
+		
+		addVirtualPad(UP_DOWN, A_B);	
+				
 		super.create();
 	}
 
